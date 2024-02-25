fb4737e87a80441cf1b3babec507ee6076306adf
Nickeem <vacation1478@gmail.com>
2024-02-25 19:17:11 -0400

    improved code logic and implemented import logger

--- App.py
+++ App.py
@@ -4,31 +4,56 @@ import pytz
 
 from Model.MassyProductModel import MassyProductModel
 from Model.MassyPriceModel import MassyPriceModel
+from Model.ImportsModel import ImportsModel
 from Scrapers.MassyScraper import MassyScraper
+from Handlers.Logger import Logger
 
 
 class App:
     timezone = 'America/Barbados'
     barbados_timezone = pytz.timezone(timezone)
 
+    LOGS_PATH = './Logs'
+
+    def __init__(self):
+        datestamp = str(datetime.datetime.utcnow().date())
+        error_log_name = 'MASSY_IMPORTS_ERRORS_'
+        import_log_name = 'MASSY_IMPORTS_'
+        error_log_file_name = error_log_name + datestamp
+        import_log_file_name = import_log_name + datestamp
+
+        self.ERROR_LOGGER = Logger(self.LOGS_PATH, error_log_file_name)
+        self.IMPORT_LOGGER = Logger(self.LOGS_PATH, import_log_file_name)
+
     def run(self):
         product_model = MassyProductModel()
         price_model = MassyPriceModel()
+        imports_model = ImportsModel()
         # product_model.remove_all()
         scraper = MassyScraper()
         main_menu_info, submenu_info = scraper.get_sources()
+
+        # scraping stats
         total_sources = 0
+        total_products = 0
+        new_products_scraped = 0
+        price_updated = 0
+        price_update_skipped = 0
+        products_skipped = 0
+
         failed_scraped = 0
+        start_time = str(datetime.datetime.utcnow())
+
         for menu_item in submenu_info:
-            '''
-            ISSUES:
-                products with discounts gives and error when the price is scraped
-                products with no images give errors
-            '''
+            if total_sources >= 50:
+                break
             product_link = menu_item['link']
             product_sources = scraper.get_product_sources(product_link)
             for source in product_sources:
-                total_sources += 1
+                # dev clause
+                if total_sources >= 50:
+                    break
+
                 try:
 
                     info = scraper.get_product_info(source)
@@ -41,15 +66,38 @@ class App:
 
                     info['link_to_product'] = source
 
-                    print(f"added product {info['product_name']} ")
-                    product_model.add_product(info['product_name'], info['categories'], info['link_to_product'], info['img_source'])
-                    product_id = product_model.getProducts(info['product_name'])[0].collection_id['id']
-                    price_model.add_product_price(product_id, info['price'], utc_now_s)
+                    product_was_added = product_model.add_product(info['product_name'], info['categories'],
+                                                                  info['link_to_product'], info['img_source'])
+                    if product_was_added:
+                        self.IMPORT_LOGGER.log(f"added product {info['product_name']}. {info['link_to_product']}")
+                        new_products_scraped += 1
+                    else:
+                        self.IMPORT_LOGGER.log(f"Product already exists and was not added: {info['product_name']}. {info['link_to_product']}")
+                        products_skipped += 1
 
-                except:
+                    product_id = product_model.getProducts(info['product_name'])[0].collection_id['id']
+
+                    was_price_updated = price_model.wasProductUpdated(product_id, utc_now)
+                    if not was_price_updated:
+                        price_model.add_product_price(product_id, info['price'], utc_now_s)
+                        price_updated += 1
+                    else:
+                        price_update_skipped += 1
+
+                except Exception as err:
                     failed_scraped += 1
-                    print(f'Error parsing product info {source}')
+                    message = f'Error parsing product info for {source}. Error: {err}'
+                    self.ERROR_LOGGER.log(message)
+                    # print(f'Error parsing product info {source}')
+                finally:
+                    total_sources += 1
                 # print(info)
+        end_time = str(datetime.datetime.utcnow())
+
+        # log import job stats
+        imports_model.add_import_info(start_time, end_time, new_products_scraped, failed_scraped,
+                                      products_skipped, price_updated, price_update_skipped, total_products,
+                                      product_model.DATA_SOURCE)
 
         print(
             f'Stats:\nTotal products: {total_sources}\nFailed to Scrape: {failed_scraped}\nScrape %:{((total_sources - failed_scraped) / total_sources) * 100}% ')
@@ -60,7 +108,6 @@ class App:
         for source in product_sources:
             print(source)
 
+
 app = App()
 app.run()
-
-
