d90470c0af9e73a5fa91a33f640dbbecdd5bc554
Nickeem <vacation1478@gmail.com>
2024-02-25 19:15:32 -0400

    commented potential error

--- Model/MassyPriceModel.py
+++ Model/MassyPriceModel.py
@@ -1,5 +1,5 @@
 from pocketbase import PocketBase
-
+from datetime import datetime, timedelta
 
 class MassyPriceModel:
     HOST = '127.0.0.1'
@@ -29,17 +29,34 @@ class MassyPriceModel:
         result = self.price_collection.get_list(page, per_page, filter_str)
         return result.items
 
+    def wasProductUpdated(self, product_id, date):
+        # Get current date and time
+        # now = datetime.datetime.utcnow().date()
+
+        # Get start of today
+        start_of_day = datetime(date.year, date.month, date.day)
+
+        # Get start of tomorrow by adding one day to start of today
+        start_of_next_day = start_of_day + timedelta(days=1)
+
+        # Get one second before tomorrow
+        one_second_before_next_day = start_of_next_day - timedelta(seconds=1)
+        filter_str = {"filter": f' product_id = "{product_id}" && datestamp >= "{str(start_of_day)}" && datestamp <= "{str(one_second_before_next_day)}" '}
+        result = self.price_collection.get_list(page=1, per_page=30, query_params=filter_str)
+        return len(result.items) > 0
+
     def update_product_price(self, id, price, datestamp):
         # self.price_collection.update(id, )
         pass
 
-    def update_product_price_by_name(self, product_name, price):
-        result = self.getProducts(product_name)
-        self.update_product_price(result[0].collection_id['id'], price)
+    # def update_product_price_by_name(self, product_name, price):
+    #     result = self.getProducts(product_name) this will cause an error
+    #     self.update_product_price(result[0].collection_id['id'], price)
 
     # def remove_all(self):
     #     self.price_collection.delete
 
 
-model = MassyPriceModel()
-model.add_product_price('w2136gsbltn9tg9', 5.75, '2024-02-07 01:39:56.713604')
+# model = MassyPriceModel()
+# print(model.wasProductUpdated("0z2gtxov6t2p3zx", datetime.utcnow().date()))
+# model.add_product_price('w2136gsbltn9tg9', 5.75, '2024-02-07 01:39:56.713604')
